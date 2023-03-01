c7771ad01ea3fea41e6f8bea01be4bff0f1d7db5
Danukadealwis <danuka.dealwis99@gmail.com>
2023-03-02 00:06:09 +1300

    Compiling, need to make math do big numbers

--- large-numbers/large-numbers.hpp
+++ large-numbers/large-numbers.hpp
@@ -5,24 +5,17 @@
 #include <iostream>
 #include <cmath>
 #define USER_INPUT_SIZE 10
-namespace LargeNumbers {
-    class LargeNumberCalculator{
-    private:
         enum class InputStatus {
             VALID,
             NO_INPUT,
 //            DECIMAL_VALUE,
             INVALID_INPUT,
             OUT_OF_RANGE,
-            MULTIPLE_INPUTS,
-
+            MULTIPLE_INPUTS
         };
 
-        static InputStatus GetInputNumber(uint64_t &input_number);
-        static void DisplayMessage(InputStatus input_status);
-        static uint64_t GetReverseNumber(uint64_t number);
-//        long SquareAndPrint(const uint64_t &value_a, const uint64_t &value_b);
-        uint64_t SquareInteger(uint64_t value, uint64_t exponent);
-
-    };
-}
+        static InputStatus GetInputNumber(unsigned long &input_number);
+        void DisplayMessage(InputStatus input_status);
+        static unsigned long GetReverseNumber(unsigned long number);
+//        long SquareAndPrint(const unsigned long &value_a, const unsigned long &value_b);
+        unsigned long CalculateResult(unsigned long value, unsigned long exponent);
